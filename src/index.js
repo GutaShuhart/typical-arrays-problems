67205e9d9cd320870f8235eb39ea0fefba9892d4
Liza <tala.91@mail.ru>
2021-08-19 20:38:44 +0400

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0 )  return 0;
+    const minNumber = Math.min(...array);
+    return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0 )  return 0;
+    const maxNumber = Math.max(...array);
+    return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0 )  return 0;
+    let sum = 0;
+    let count = array.length;
+    for (let i = 0; i < count; i++) {
+        sum += array[i];
+    };
+    return sum/count;
 }
